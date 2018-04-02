88d749720819cdd767e52ca987961eccd2ee6e75
edgarhovhannisyan <32413115+edgarhovhannisyan@users.noreply.github.com>
2018-04-02 18:22:03 +0400

    +

--- views/asset/script.js
+++ views/asset/script.js
@@ -1,21 +1,21 @@
-	const ArmRegions = ['Lori','Tavush','Shirak','Gegharkunik','Kotayk','Armavir','Ararat','Aragatsotn','Vayots_Dzor','Syunik','Arcax','Yerevan'];
+const ArmRegions = ['Lori','Tavush','Shirak','Gegharkunik','Kotayk','Armavir','Ararat','Aragatsotn','Vayots_Dzor','Syunik','Arcax','Yerevan'];
 // Defining Locations
-    const Lori = ['Vanadzor','Alaverdi','Stepanavan','Spitak','Axtala','Shamlux','Tashir'];
-    const Tavush = ['Ijevan','Dilijan','Berd','Noyemberyan','Ayrum'];
-    const Shirak = ['Gyumri','Artik','Maralik']
-    const Gegharkunik = ['Sevan','Chambarak','Vardenis','Martuni','Gavar']
-    const Kotayk = ['Hrazdan','Abovyan','Tsaxkadzor','Charencavan','Nor_Hachin','Byureghavan']
-    const Armavir = ['Armavir','Vagharshapat']
-    const Ararat = ['Artashat','Ararat','Vedi','Masis']
-    const Aragatsotn = ['Ashtarak','Aparan','Talin']
-    const Vayots_Dzor = ['Yeghegnadzor','Vayq','Jermuk',]
-    const Syunik = ['Kapan','Goris','Qajaran','Sisian','Megri','Dastakert']
+const Lori = ['Vanadzor','Alaverdi','Stepanavan','Spitak','Axtala','Shamlux','Tashir'];
+const Tavush = ['Ijevan','Dilijan','Berd','Noyemberyan','Ayrum'];
+const Shirak = ['Gyumri','Artik','Maralik']
+const Gegharkunik = ['Sevan','Chambarak','Vardenis','Martuni','Gavar']
+const Kotayk = ['Hrazdan','Abovyan','Tsaxkadzor','Charencavan','Nor_Hachin','Byureghavan']
+const Armavir = ['Armavir','Vagharshapat']
+const Ararat = ['Artashat','Ararat','Vedi','Masis']
+const Aragatsotn = ['Ashtarak','Aparan','Talin']
+const Vayots_Dzor = ['Yeghegnadzor','Vayq','Jermuk',]
+const Syunik = ['Kapan','Goris','Qajaran','Sisian','Megri','Dastakert']
 // End Locations
 
 $('.country_select').on('change', function () {
 	//$( ".country_select option" ).remove();
 	var country = $( this ).val();
-    switch(country){
+	switch(country){
 		case 'Armenia':
 		// Create an array containing regions of Armenia
 		regions = ArmRegions;
@@ -25,8 +25,8 @@ $('.country_select').on('change', function () {
 		break;
 		default:
 		$.alert({
-		title: 'Alert!',
-		content: 'Specify the country !',
+			title: 'Alert!',
+			content: 'Specify the country !',
 		});
 	}
 
@@ -36,7 +36,7 @@ $('.country_select').on('change', function () {
 	}
 
 	for (var i = 0; i < regions.length; i++){
-    	$(this).parent().next().next().find(".region_select").append("<option value="+regions[i]+">"+regions[i]+"</option>");
+		$(this).parent().next().next().find(".region_select").append("<option value="+regions[i]+">"+regions[i]+"</option>");
 	}
 })
 
@@ -61,7 +61,7 @@ $('.region_select').on('change', function() {
 	// var geocoder = new google.maps.Geocoder();
 
 	// Get Regions
-    switch(region){
+	switch(region){
 		case 'Lori':
 		city = Lori;
 		break;
@@ -76,20 +76,20 @@ $('.region_select').on('change', function() {
 		break;
 		case 'Kotayk':
 		city = Kotayk;
-	    break;
-	    case 'Armavir':
+		break;
+		case 'Armavir':
 		city = Armavir;
-	    break;
-	    case 'Ararat':
+		break;
+		case 'Ararat':
 		city = Ararat;
-	    break;
-	    case 'Aragatsotn':
+		break;
+		case 'Aragatsotn':
 		city = Aragatsotn;
-	    break;
-	    case 'Vayots_Dzor':
+		break;
+		case 'Vayots_Dzor':
 		city = Vayots_Dzor;
-	    break;
-	    case 'Syunik':
+		break;
+		case 'Syunik':
 		city = Syunik;
 		break;
 		case 'defval':
@@ -111,7 +111,7 @@ $('.region_select').on('change', function() {
 	}
 	$(".city_select").append("<option value='defval' selected='selected'>City</option>");
 	for(var i = 0; i < city.length; i++){
-    	$(".city_select").append("<option value="+city[i]+">"+city[i]+"</option>");
+		$(".city_select").append("<option value="+city[i]+">"+city[i]+"</option>");
 	}
 })
 
@@ -140,53 +140,53 @@ $('.city_select').on('change', function () {
 
 });
 
-    // File Transfer
-	filer = [];
-	var numPhotos;
-    var fileButton = document.getElementById('fileButton');
+// File Transfer
+filer = [];
+var numPhotos;
+var fileButton = document.getElementById('fileButton');
 
-    // Listen for file selection
-	$("#fileButton").on("click", function(){
-	  	fileButton.addEventListener('change', function(e) {
-			$('#progress > *').remove();
-			$('#upload_pics > *').remove();
+// Listen for file selection
+$("#fileButton").on("click", function(){
+	fileButton.addEventListener('change', function(e) {
+		$('#progress > *').remove();
+		$('#upload_pics > *').remove();
 
-			// Get files
-			filer=e.target.files;
+		// Get files
+		filer=e.target.files;
 
-			numPhotos = e.target.files.length;
-			var uploader=[];
+		numPhotos = e.target.files.length;
+		var uploader=[];
 
-			for(i = 0; i < numPhotos; i++){
-				var uploaderID='uploader'+i;
+		for(i = 0; i < numPhotos; i++){
+			var uploaderID='uploader'+i;
 
-				var myid = "myimg"+i;
-				$("#upload_file #upload_pics").append("<div class='col-xs-6 col-md-3'><a class='thumbnail'><img src='' id="+myid+" alt='uploading'></a></div>")
-				document.getElementById(myid).src = URL.createObjectURL(event.target.files[i]);
+			var myid = "myimg"+i;
+			$("#upload_file #upload_pics").append("<div class='col-xs-6 col-md-3'><a class='thumbnail'><img src='' id="+myid+" alt='uploading'></a></div>")
+			document.getElementById(myid).src = URL.createObjectURL(event.target.files[i]);
 
-				uploader[i] = document.getElementById(uploaderID);
-			}
-		})
-
-
+			uploader[i] = document.getElementById(uploaderID);
+		}
+	})
 
 
-	})  // End File Selection
 
 
-	// Listen for file selection
+})  // End File Selection
+
+
+// Listen for file selection
 function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      $('.diff_origin_photo_viewer img').attr('src', e.target.result);
-    }
-    reader.readAsDataURL(input.files[0]);
-  }
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			$('.diff_origin_photo_viewer img').attr('src', e.target.result);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
 }
 
 $("#diff_origin_photo").change(function() {
-  readURL(this);
+	readURL(this);
 });
 
 
@@ -197,7 +197,7 @@ var firebaseRef = firebase.database().ref();
 $("#send_data").click(function(){
 
 	//getting values
-  var fields = getValues();
+	var fields = getValues();
 
 	// Validation
 	validate(fields);
@@ -206,44 +206,44 @@ $("#send_data").click(function(){
 	idPath = "checkpoints/" + fields.country + "/" + fields.region + "/" + fields.city + "/" + fields.id;
 
 	// Getting Json tree to firebase
-    var JsonToFirebase = {
-	    location: fields.location_array,
-			checkpoints_type: fields.type,
-			scores: {
-				score_type_1:fields.score
-			},
-			marker_url: fields.marker_url,
-			marker_radius: fields.marker_radius,
-			difficulty: fields.difficulty,
-			numphotos: numPhotos,
-			title: {
-				arm_title: fields.arm_title,
-				rus_title: fields.rus_title,
-				eng_title: fields.eng_title,
-			},
-			description: {
-				arm_desc: fields.arm_desc,
-				rus_desc: fields.rus_desc,
-				eng_desc: fields.eng_desc,
-			},
-    }
+	var JsonToFirebase = {
+		location: fields.location_array,
+		checkpoints_type: fields.type,
+		scores: {
+			score_type_1:fields.score
+		},
+		marker_url: fields.marker_url,
+		marker_radius: fields.marker_radius,
+		difficulty: fields.difficulty,
+		numphotos: numPhotos,
+		title: {
+			arm_title: fields.arm_title,
+			rus_title: fields.rus_title,
+			eng_title: fields.eng_title,
+		},
+		description: {
+			arm_desc: fields.arm_desc,
+			rus_desc: fields.rus_desc,
+			eng_desc: fields.eng_desc,
+		},
+	}
 
-    // Inserting to firebase DB
-    firebaseRef.child(idPath).set(JsonToFirebase);
-    //
-    // firebaseRef.child(idPath + "/location").set(fields.location_array);
-    // firebaseRef.child(idPath + "/checkpoints_type").set(fields.type);
-    // firebaseRef.child(idPath + "/scores").child("score_type_1").set(fields.score);
-    // firebaseRef.child(idPath + '/marker_url').set(fields.marker_url);
-    // firebaseRef.child(idPath + '/marker_radius').set(marker_radius);
-    // firebaseRef.child(idPath + '/difficulty').set(fields.difficulty);
-    // firebaseRef.child(idPath + '/numphotos').set(numPhotos);
-    // firebaseRef.child(idPath + "/title/arm_title").set(fields.arm_title);
-    // firebaseRef.child(idPath + "/title/rus_title").set(fields.rus_title);
-    // firebaseRef.child(idPath + "/title/eng_title").set(fields.eng_title);
-    // firebaseRef.child(idPath + "/description/arm_desc").set(fields.arm_desc);
-    // firebaseRef.child(idPath + "/description/rus_desc").set(fields.rus_desc);
-    // firebaseRef.child(idPath + "/description/eng_desc").set(fields.eng_desc);
+	// Inserting to firebase DB
+	firebaseRef.child(idPath).set(JsonToFirebase);
+	//
+	// firebaseRef.child(idPath + "/location").set(fields.location_array);
+	// firebaseRef.child(idPath + "/checkpoints_type").set(fields.type);
+	// firebaseRef.child(idPath + "/scores").child("score_type_1").set(fields.score);
+	// firebaseRef.child(idPath + '/marker_url').set(fields.marker_url);
+	// firebaseRef.child(idPath + '/marker_radius').set(marker_radius);
+	// firebaseRef.child(idPath + '/difficulty').set(fields.difficulty);
+	// firebaseRef.child(idPath + '/numphotos').set(numPhotos);
+	// firebaseRef.child(idPath + "/title/arm_title").set(fields.arm_title);
+	// firebaseRef.child(idPath + "/title/rus_title").set(fields.rus_title);
+	// firebaseRef.child(idPath + "/title/eng_title").set(fields.eng_title);
+	// firebaseRef.child(idPath + "/description/arm_desc").set(fields.arm_desc);
+	// firebaseRef.child(idPath + "/description/rus_desc").set(fields.rus_desc);
+	// firebaseRef.child(idPath + "/description/eng_desc").set(fields.eng_desc);
 
 	// Put Images To Storage
 	var storageRef = [];
@@ -258,20 +258,20 @@ $("#send_data").click(function(){
 
 	document.getElementById("add_checkpoint").reset();
 	$.alert({
-    	title: 'Alert!',
-    	content: 'Checkpoint added succesfully!',
+		title: 'Alert!',
+		content: 'Checkpoint added succesfully!',
 	});
 
 }) // End send data
 
 /* Get data from DB */
 $('#cities2').on('change', function() {
-     $( ".point_row_in" ).remove();
-    var country2 = $("#country2").val().toLowerCase().slice(0,3);
-    var region2 = $("#regions2").val().toLowerCase().slice(0,3);
-    city2 = $("#cities2").val().toLowerCase().slice(0,3);
-    const cityRef = firebaseRef.child("checkpoints/" + country2 +"/"+ region2 +"/"+ city2 +"/");
-    idRef = cityRef.orderByKey().limitToFirst(1000);
+	$( ".point_row_in" ).remove();
+	var country2 = $("#country2").val().toLowerCase().slice(0,3);
+	var region2 = $("#regions2").val().toLowerCase().slice(0,3);
+	city2 = $("#cities2").val().toLowerCase().slice(0,3);
+	const cityRef = firebaseRef.child("checkpoints/" + country2 +"/"+ region2 +"/"+ city2 +"/");
+	idRef = cityRef.orderByKey().limitToFirst(1000);
 })
 
 $(".questions_table").on("change", function(){
@@ -298,33 +298,33 @@ $(".checkpoints_table").on("change", function(){
 
 	idRef.on("value", function(snapshot){
 		$( ".point_row_in" ).remove();
-			var allCheckpointsInCity = snapshot.val();
-			var checkpointIds = Object.getOwnPropertyNames(allCheckpointsInCity);
-			var num_checkpoints = checkpointIds.length;
-			var clearfix = 0;
-			for(i = 0; i <= num_checkpoints-1; i++) {
-				var checkpointIdss = Object.getOwnPropertyNames(allCheckpointsInCity);
-				var checkpointName=checkpointIds[i];
-				temporary_checkpoint_object = allCheckpointsInCity[checkpointName];
-				var temporary_title = temporary_checkpoint_object.title.eng_title;
-				var temporary_desc = temporary_checkpoint_object.description.eng_desc;
-				temporary_desc = temporary_desc.substring(0, 185) + " ...";
-				var head_pic_name = temporary_checkpoint_object.head_pic_name;
+		var allCheckpointsInCity = snapshot.val();
+		var checkpointIds = Object.getOwnPropertyNames(allCheckpointsInCity);
+		var num_checkpoints = checkpointIds.length;
+		var clearfix = 0;
+		for(i = 0; i <= num_checkpoints-1; i++) {
+			var checkpointIdss = Object.getOwnPropertyNames(allCheckpointsInCity);
+			var checkpointName=checkpointIds[i];
+			temporary_checkpoint_object = allCheckpointsInCity[checkpointName];
+			var temporary_title = temporary_checkpoint_object.title.eng_title;
+			var temporary_desc = temporary_checkpoint_object.description.eng_desc;
+			temporary_desc = temporary_desc.substring(0, 185) + " ...";
+			var head_pic_name = temporary_checkpoint_object.head_pic_name;
 
-				if (clearfix == 4) {
-					$("#check").append("<div class='clearfix'></div>")
-					clearfix = 0;
-				}
-
-				$("#check").append("<div class='col-md-3 col-sm-6 col-sm-offset-0 point_row_in' style='padding:10px;'><div class='point_row'><p class='image_block'><img src='"+head_pic_name+"' alt='image.jpg' onerror='this.remove(this);' /><h3>"+temporary_title+"</h3><p class='description_block'>" + temporary_desc + "</p><p class='tool_block'><input class='aass btn btn-default table_button' value='Edit' onclick='get_point_fields(this);' type='button' data-toggle='modal' data-target='#myModal3' data-id='"+checkpointIdss[i]+"''><input class='btn btn-default table_button' type='button' data-id='"+checkpointIdss[i]+"'' onclick='remove_checkpoint(this)' value='Remove' /></p></div></div>");
-				clearfix ++;
+			if (clearfix == 4) {
+				$("#check").append("<div class='clearfix'></div>")
+				clearfix = 0;
 			}
+
+			$("#check").append("<div class='col-md-3 col-sm-6 col-sm-offset-0 point_row_in' style='padding:10px;'><div class='point_row'><p class='image_block'><img src='"+head_pic_name+"' alt='image.jpg' onerror='this.remove(this);' /><h3>"+temporary_title+"</h3><p class='description_block'>" + temporary_desc + "</p><p class='tool_block'><input class='aass btn btn-default table_button' value='Edit' onclick='get_point_fields(this);' type='button' data-toggle='modal' data-target='#myModal3' data-id='"+checkpointIdss[i]+"''><input class='btn btn-default table_button' type='button' data-id='"+checkpointIdss[i]+"'' onclick='remove_checkpoint(this)' value='Remove' /></p></div></div>");
+			clearfix ++;
+		}
 	})
 })
 
 $('#country2').on('change',function() {
-    $( ".point_row_in" ).remove();
+	$( ".point_row_in" ).remove();
 })
 $('#regions2').on('change', function() {
-    $( ".point_row_in" ).remove();
+	$( ".point_row_in" ).remove();
 })
